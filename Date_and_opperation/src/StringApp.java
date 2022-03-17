e8d9a83596f02dbc56dd3d60e099693ed751d134
CodingHongDong <101644470+CodingHongDong@users.noreply.github.com>
2022-03-17 16:56:17 +0900

    Add files via upload

---
+++ Date_and_opperation/src/StringApp.java
@@ -0,0 +1,19 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		
+		// String VS Character
+		System.out.println("Hello World"); // String
+		System.out.println('H'); // Character
+
+		System.out.println("Hello "
+				+ "World");
+		// \n << new line
+		System.out.println("Hello \nWorld");
+		
+		// escape
+		System.out.println("Hello \"World\""); // Hello "World"
+	}
+
+}
