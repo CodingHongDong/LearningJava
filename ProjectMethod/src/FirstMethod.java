76d14023eb844632bf908d551f43e30f9b6da3f7
CodingHongDong <101644470+CodingHongDong@users.noreply.github.com>
2022-03-23 20:34:18 +0900

    메서드 기본
    메서드의 기본형식
    메서드의 입력, 출력, 활용

---
+++ ProjectMethod/src/FirstMethod.java
@@ -0,0 +1,12 @@
+
+public class FirstMethod {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Method");
+		System.out.println(Math.floor(1.1)); //1.0
+		System.out.println(Math.ceil(1.1)); //2.0
+		
+	}
+
+}
---
+++ ProjectMethod/src/WhyMethod.java
@@ -0,0 +1,35 @@
+
+public class WhyMethod {
+	
+	public static void main(String[] args) {
+		
+					//인자 = argument
+		printTwoTimes("a", "-");
+		System.out.println(twoTimes("a", "-"));
+		printTwoTimes("a", "*");
+		printTwoTimes("a", "&");
+		printTwoTimes("b", "!");
+
+	}
+	
+	public static String twoTimes(String text, String delimiter) {
+		String out = "";
+		out = out + delimiter + "\n";
+		out = out + text + "\n";
+		out = out + text + "\n";
+		return out;
+	}
+	
+									//매개변수 = parameter
+	public static void printTwoTimes(String text, String delimiter) {
+		System.out.println(delimiter);
+		System.out.println(text);
+		System.out.println(text);
+	}
+	public static void writeFileTwoTimes(String text, String delimiter) {
+		System.out.println(delimiter);
+		System.out.println(text);
+		System.out.println(text);
+	}
+
+}
